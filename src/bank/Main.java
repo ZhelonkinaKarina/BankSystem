a33e64daf1843362f9a08ac6e669f687f80ae683
ZhelonkinaKarina <126237126+ZhelonkinaKarina@users.noreply.github.com>
2023-12-20 15:33:57 +0200

    Update Main.java

--- src/bank/Main.java
+++ src/bank/Main.java
@@ -1,13 +1,13 @@
 package bank;
 
-public class Main {
-public static void main(String[] args) {
-	 // Створення екземплярів класів
+class Main {
+	public static void main(String[] args) {
+	 //Створення екземплярів класів
     Client client1 = new Client("Марта", 25, "Жіноча", 65000.0);
     Account account1 = new Account("A123", "Вкладний (депозитний)", 20000.0);
     Transaction transaction1 = new Transaction("T001", "Переказ коштів");
 
-    // Виклик методів
+    //Виклик методів
     client1.displayInfo();
     account1.displayInfo();
 
@@ -15,6 +15,35 @@ public static void main(String[] args) {
     transaction1.executeTransaction(client1, 11500.0);
 
     account1.displayInfo();
-}
+    
+    Transaction.printTransactionInfo("T002", "Виведення коштів");  //Виклик статичного методу
+    
+    //Ініціалізація змінних різними варіантами
+    Client client2 = new Client("Петро", 30, "Чоловіча", 75000.0, true); //Варіант ініціалізації 1
+    Client client3 = new Client("Анна", 55000.0, false, "Жіноча", 28); //Варіант ініціалізації 2
+    Client client4 = new Client("Катерина", "Жіноча", 34, 95000.0, true); //Варіант ініціалізації 3
+    Client client5 = new Client("Максим", true, "Чоловіча", 51, 60000.0); //Варіант ініціалізації 4
+    Account account2 = new Account("A124", "Вкладний (депозитний)", 30000.0);
+    Account account3 = new Account("A125", "Вкладний (депозитний)", 40000.0);
+    Transaction transaction2 = new Transaction("T002", "Зняття коштів");
+    Transaction transaction3 = new Transaction("T003", "Переказ коштів");
+    
+    
+    //Виклик новостворених методів
+    Account account4 = new Account("A123", "Вкладний (депозитний)", 20000.0);
+    Account account5 = new Account("A124", "Вкладний (депозитний)", 30000.0);
 
+    //Виклик методу, що приймає екземпляр того ж класу в якості параметру
+    account1.performOperations(account2);
+
+    //Виклик методу посилкового типу
+    Account updatedAccount = account1.updateAccount(25000.0);
+
+    //Виведення інформації про оновлений об'єкт
+    System.out.println("Оновлений баланс рахунку 1: " + updatedAccount.getBalance());
+	
+    //Виклик рекурсивного методу
+    Client client = new Client("Іван", 35, "Чоловіча", 50000.0);
+    client.recursiveMethod(3, "Застосування рекурсії");
+	}
 }
