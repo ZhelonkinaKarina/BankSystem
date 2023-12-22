e45f6df23ce7f8f332ba65a42b9ece1b8fb64c1a
ZhelonkinaKarina <126237126+ZhelonkinaKarina@users.noreply.github.com>
2023-12-22 08:21:48 +0200

    Create AdditionalClass.java

---
+++ src/bank/AdditionalClass.java
@@ -0,0 +1,25 @@
+package bank;
+
+class AdditionalClass {
+	
+	//Статичні змінні з ініціалізацією під час оголошення та статичним блоком
+    private static int staticVariable1 = 10;
+    private static int staticVariable2;
+
+    static {
+        staticVariable2 = 20;
+    }
+
+    //Перший статичний метод
+    static void firstStaticMethod() {
+        System.out.println("Перший статичний метод. Змінна 1: " + staticVariable1 + ", Змінна 2: " + staticVariable2);
+    }
+
+    //Другий статичний метод
+    static void secondStaticMethod() {
+        //Використання результатів роботи першого методу
+        firstStaticMethod();
+        System.out.println("Другий статичний метод.");
+    }
+
+}
